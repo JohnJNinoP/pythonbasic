f6f53165db7618b6b7725161f46363e6456309db
JohnJNinoP <Ingenieromaster.desarrollo@interrapidisimo.com>
2021-08-02 15:43:47 -0500

    Fields

---
+++ basic2/aproximacion.py
@@ -0,0 +1,20 @@
+def run():
+    objetivo = int(input('Escoge un numero'))
+    epsilon = 0.01
+    paso  = epsilon**2
+    respuesta = 0.0
+    print(paso)
+    print(abs(respuesta**2 - objetivo ))
+    while abs(respuesta**2 -objetivo ) >= epsilon  and respuesta <= objetivo:
+        respuesta += paso 
+
+    if abs(respuesta**2 -objetivo ) >= epsilon :
+        print(f'No se encontro la raiz cuadrada del objetivo')
+    else:
+        print(f'la raiz cuadrada es {respuesta}')
+
+
+
+
+if __name__ == '__main__':
+    run()
