8c87dd6bb71b131ed0a2d00a771ddc17ffcbbff6
johnjaironp <jjninop@gmail.com>
2022-01-21 10:35:33 -0500

    field turtle

---
+++ line_command_sale/main.py
@@ -0,0 +1,9 @@
+from http import client
+
+
+clients = "pablo,ricardo"
+
+if __name__ == '__main__':
+    clients += ",david"
+    print(clients)
+    pass
--- parctico_crud/turtle_square.py
+++ parctico_crud/turtle_square.py
@@ -1,8 +1,25 @@
 import turtle
 
+
 window = turtle.Screen()
+window.title("test")
+
 tortuga = turtle.Turtle()
 
+#tortuga.hideturtle()
+
 tortuga.forward(100)
+tortuga.right(90)
+
+tortuga.forward(100)
+tortuga.right(90)
+
+tortuga.forward(100)
+tortuga.right(90)
+
+tortuga.forward(100)
+tortuga.right(90)
+
+tortuga.goto(100,-100), 
 
 window.mainloop()
